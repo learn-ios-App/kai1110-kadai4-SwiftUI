96d5191e6f0b947a2bff47ea7e771965ff24d2d3
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2022-11-20 07:19:31 +0900

    commit

--- kai1110-kadai4/ContentView.swift
+++ kai1110-kadai4/ContentView.swift
@@ -2,25 +2,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var countNumber = 0
+    @State var countNumber = 0
     
     var body: some View {
         VStack(spacing: 30) {
             Text("\(countNumber)")
                 .font(.title)
-            
-            Button(action: {
+            ButtonView(countNumber: $countNumber, text: "+1", checkAction: true)
+            ButtonView(countNumber: $countNumber, text: "clear", checkAction: false)
+        }
+    }
+}
+
+struct ButtonView: View {
+    @Binding var countNumber: Int
+    let text: String
+    let checkAction: Bool
+    
+    var body: some View {
+        Button(action: {
+            if checkAction {
                 countNumber += 1
-            }) {
-                Text("+1")
-                    .font(.title)
             }
-            Button(action: {
+            else {
                 countNumber = 0
-            }) {
-                Text("Clear")
-                    .font(.title)
             }
+        }) {
+            Text(text)
+                .font(.title)
         }
     }
 }
