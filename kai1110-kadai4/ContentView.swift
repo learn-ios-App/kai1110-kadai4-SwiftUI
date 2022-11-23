0968c9041982650cd421f0cd68ec1ace8055babb
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2022-11-23 21:40:31 +0900

    commit

--- kai1110-kadai4/ContentView.swift
+++ kai1110-kadai4/ContentView.swift
@@ -8,31 +8,23 @@ struct ContentView: View {
         VStack(spacing: 30) {
             Text("\(countNumber)")
                 .font(.title)
-            ButtonView(countNumber: $countNumber, text: "+1", checkAction: true)
-            ButtonView(countNumber: $countNumber, text: "clear", checkAction: false)
+            Button(action: {
+                countNumber += 1
+            }) {
+                Text("+1")
+                    .font(.title)
+            }
+            Button(action: {
+                countNumber = 0
+            }) {
+                Text("clear")
+                    .font(.title)
+            }
         }
     }
 }
 
-struct ButtonView: View {
-    @Binding var countNumber: Int
-    let text: String
-    let checkAction: Bool
-    
-    var body: some View {
-        Button(action: {
-            if checkAction {
-                countNumber += 1
-            }
-            else {
-                countNumber = 0
-            }
-        }) {
-            Text(text)
-                .font(.title)
-        }
-    }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
